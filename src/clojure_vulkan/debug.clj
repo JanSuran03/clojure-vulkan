0157c08c00b3cb77d4d870a006bbf3e80d97c894
JanSuran03 <suran.orgpad@gmail.com>
2022-07-25 23:37:54 +0200

    Debug messenger setup.

--- src/clojure_vulkan/debug.clj
+++ src/clojure_vulkan/debug.clj
@@ -1,10 +1,12 @@
 (ns clojure-vulkan.debug
   (:require [clojure-vulkan.validation-layers :as validation-layers]
-            [clojure-vulkan.util :as util :refer [nullptr]])
-  (:import (org.lwjgl.vulkan VkDebugUtilsMessengerCallbackDataEXT VkDebugUtilsMessengerCreateInfoEXT VK13 EXTDebugUtils VkDebugUtilsMessengerCallbackEXTI)
-           (org.lwjgl.system MemoryStack)))
+            [clojure-vulkan.util :as util :refer [nullptr]]
+            [clojure-vulkan.instance :as instance])
+  (:import (org.lwjgl.vulkan VkDebugUtilsMessengerCallbackDataEXT VkDebugUtilsMessengerCreateInfoEXT VK13 EXTDebugUtils VkDebugUtilsMessengerCallbackEXTI VkInstance VkAllocationCallbacks)
+           (org.lwjgl.system MemoryStack)
+           (java.nio LongBuffer)))
 
-(def ^long debug-messenger)
+(def ^long debug-messenger -1)
 
 (def debug-callback
   (reify VkDebugUtilsMessengerCallbackEXTI
@@ -13,6 +15,12 @@
         (binding [*out* *err*]
           (println "Validation layer callback: " (.pMessageString callback-data))))
       false)))
+
+(defn create-debug-messenger-extension [^VkInstance instance ^VkDebugUtilsMessengerCreateInfoEXT create-info
+                                        ^VkAllocationCallbacks allocation-callbacks ^LongBuffer debug-messenger-ptr]
+  (if (= (VK13/vkGetInstanceProcAddr instance "vkCreateDebugUtilsMessengerEXT") nullptr)
+    false
+    (EXTDebugUtils/vkCreateDebugUtilsMessengerEXT instance create-info allocation-callbacks debug-messenger-ptr)))
 
 (defn setup-debug-messenger []
   (when validation-layers/*enable-validation-layers*
@@ -27,4 +35,8 @@
                                           EXTDebugUtils/VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT
                                           EXTDebugUtils/VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT))
               (.pfnUserCallback debug-callback)
-              (.pUserData nullptr))]))))
+              (.pUserData nullptr))
+            ^LongBuffer debug-messenger-ptr (.longs stack VK13/VK_NULL_HANDLE)]
+        (if (create-debug-messenger-extension instance/vulkan-instance create-info nil debug-messenger-ptr)
+          (alter-var-root #'debug-messenger (constantly (.get debug-messenger-ptr 0)))
+          (throw (RuntimeException. "Failed to set up debug messenger.")))))))
