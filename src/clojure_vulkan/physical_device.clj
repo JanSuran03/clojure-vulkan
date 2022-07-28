5c543e0ec109ae5c7fd2cd0df8d728c7c0c9b099
JanSuran03 <suran.orgpad@gmail.com>
2022-07-28 03:11:30 +0200

    Some boilerplate code for picking the best physical device.

--- src/clojure_vulkan/physical_device.clj
+++ src/clojure_vulkan/physical_device.clj
@@ -2,10 +2,26 @@
   (:require [clojure-vulkan.globals :refer [physical-device vulkan-instance]]
             [clojure-vulkan.util :as util])
   (:import (org.lwjgl.system MemoryStack)
-           (org.lwjgl.vulkan VK13 VkPhysicalDevice)))
+           (org.lwjgl.vulkan VK13 VkPhysicalDevice VkPhysicalDeviceFeatures VkPhysicalDeviceProperties)
+           (org.lwjgl PointerBuffer)))
 
-(defn- is-device-suitable? [^VkPhysicalDevice device]
-  )
+(defn- pick-suitable-device [^PointerBuffer devices-ptr device-count]
+  (let [devices (object-array device-count)]
+    (util/with-memory-stack-push ^MemoryStack stack
+      (dotimes [i device-count]
+        (let [device (VkPhysicalDevice. (.get devices-ptr ^long i) vulkan-instance)
+              ^VkPhysicalDeviceProperties device-properties (VkPhysicalDeviceProperties/calloc stack)
+              ^VkPhysicalDeviceFeatures device-features (VkPhysicalDeviceFeatures/calloc stack)
+              score (volatile! 0)]
+          (do (VK13/vkGetPhysicalDeviceProperties device device-properties)
+              (VK13/vkGetPhysicalDeviceFeatures device device-features)
+              (when (= (.deviceType device-properties) VK13/VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU)
+                (vswap! score + 1000))
+              (when-not (.geometryShader device-features)
+                (vreset! score 0))
+              (aset devices i (list @score device)))))
+      (sort-by first > devices)
+      (aget devices 0))))
 
 (defn pick-physical-device []
   (util/with-memory-stack-push ^MemoryStack stack
@@ -16,10 +32,6 @@
               (throw (RuntimeException. "No GPU with Vulkan support found.")))
           physical-devices-ptr (.mallocPointer stack device-count)
           _ (VK13/vkEnumeratePhysicalDevices vulkan-instance device-count-ptr physical-devices-ptr)
-          physical-device* (or (some (fn [^long i]
-                                      (let [device (VkPhysicalDevice. (.get physical-devices-ptr i) vulkan-instance)]
-                                        (when (is-device-suitable? device)
-                                          device)))
-                                    (range device-count))
-                              (throw (RuntimeException. "No suitable GPU found.")))]
-      (alter-var-root physical-device (constantly physical-device*)))))
+          physical-device* (or (pick-suitable-device physical-devices-ptr device-count)
+                               (throw (RuntimeException. "No suitable GPU found.")))]
+      (alter-var-root #'physical-device (constantly physical-device*)))))
